1b917b465c4b31fe542186d7655ed8db2dcad83d
Rungminth <92238822+Dongmins11@users.noreply.github.com>
2024-08-07 22:24:04 +0900

    Delete MyProject/MFC봉인중 directory

--- MyProject/MFC봉인중/UpdateLib.bat
+++
@@ -1,7 +0,0 @@
-
-
-
-xcopy		/y/s		.\Engine\public\*.h				.\Reference\Headers\
-xcopy		/y/s		.\Engine\bin\*.lib				.\Reference\Librarys\
-xcopy		/y/s		.\Engine\bin\*.dll				.\Client\bin\
-xcopy	    /y/s		.\Engine\bin\*.dll				.\Tool\bin\
